3cf969cc48608ec3fcfc25bd67236922adba848f
Sunnyy09 <12001109@csepup.ac.in>
2024-10-22 22:21:50 +0530

    upload first-part

---
+++ 1-part-Basic-about-JS/index.js
@@ -0,0 +1,26 @@
+// const Student = {
+//   name: "Sunny",
+//   age: 21,
+//   cgpa: 8.2,
+//   isPass: true,
+//   address: {
+//     village: "chanarthal kalan",
+//     city: "Sirhind",
+//     Dist: "Fatehgarh sahib",
+//     state: "Punjab",
+//   },
+// };
+
+// console.log(Student.isPass);
+// console.log(Student["cgpa"]);
+// console.log(Student.address);
+
+// const product = {
+//   title: "Iphone",
+//   rating: 5,
+//   offer: 50,
+//   price: 150000,
+// };
+
+// console.log(product.price);
+// console.log(product.rating);
