4fe88c50c9b0bfe00a3e1d15d66c74444f1df016
Sunnyy09 <12001109@csepup.ac.in>
2024-10-23 13:37:41 +0530

    update Operators in JS!

---
+++ 2-part-Operators-In-JS/operators.js
@@ -0,0 +1,16 @@
+//ARITHMETIC OPERATORS
+// let a = 20;
+// let b = 10;
+
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+// console.log("a * b = ", a * b);
+// console.log(25 ** 2);
+// console.log(17 % 3);
+
+const a = 10;
+const b = 50;
+console.log((b % 9) * a);
+console.log(3 + 2 * 5);
