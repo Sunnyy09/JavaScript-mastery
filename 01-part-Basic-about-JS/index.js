9b5a6db1b20ad3ce2c7c9072ca60a5ecf6067663
Sunnyy09 <12001109@csepup.ac.in>
2024-10-27 14:17:23 +0530

    update basic about JS!

---
+++ 01-part-Basic-about-JS/index.js
@@ -0,0 +1,26 @@
+// const Student = {
+//   name: "joy",
+//   age: 21,
+//   cgpa: 8.2,
+//   isPass: true,
+//   address: {
+//     village: "abcd",
+//     city: "hns",
+//     Dist: "vrver",
+//     state: "Punjab",
+//   },
+// };
+
+// console.log(Student.isPass);
+// console.log(Student["cgpa"]);
+// console.log(Student.address);
+
+// const product = {
+//   title: "Iphone",
+//   rating: 5,
+//   offer: 50,
+//   price: 150000,
+// };
+
+// console.log(product.price);
+// console.log(product.rating);
