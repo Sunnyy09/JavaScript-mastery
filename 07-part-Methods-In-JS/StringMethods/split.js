ccb797de93399a874da33d90c895cdb872919f03
Sunnyy09 <12001109@csepup.ac.in>
2024-11-28 17:27:52 +0530

    update methods

---
+++ 07-part-Methods-In-JS/StringMethods/split.js
@@ -0,0 +1,11 @@
+//Basic Splitting:
+let fruits = "apple,banana,orange";
+console.log(fruits.split());
+
+//Space Splitting:
+let sentence = "JavaScript is fun";
+console.log(sentence.split(" "));
+
+//Character Splitting:
+let word = "hello";
+console.log(word.split(""));
