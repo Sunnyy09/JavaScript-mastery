d95b570136edb30f83bb383930f1a596e73e8b45
Sunnyy09 <12001109@csepup.ac.in>
2024-11-28 17:27:04 +0530

    update methods

---
+++ 07-part-Methods-In-JS/ArrayMethods/JoinMethod/index.js
@@ -0,0 +1,10 @@
+//Basic Join:
+const fruits = ["apple", "banana", "cherry"];
+const output = fruits.join();
+console.log(output);
+
+//Join with Space:
+console.log(fruits.join(" "));
+
+//Join with Custom Separator:
+console.log(fruits.join("-"));
